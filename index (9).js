6ab11b51632fbcdbc53fc0ddc65d08dbb95d91c0
RMNAVEEN999 <149756174+RMNAVEEN999@users.noreply.github.com>
2023-11-30 11:53:13 +0530

    Add files via upload

---
+++ index (9).js
@@ -0,0 +1,34 @@
+import {Component} from 'react'
+
+import './index.css'
+
+class ClickCounter extends Component {
+  state = {
+    count: 0,
+  }
+
+  onIncrease = () => {
+    this.setState(prevState => ({count: prevState.count + 1}))
+  }
+
+  render() {
+    const {count} = this.state
+    return (
+      <div className="container">
+        <h1 className="heading">
+          The Button
+          <br />
+          <span className="valu">{count}</span>
+        </h1>
+        <p className="descrption">click to button</p>
+        <div className="button-contaoner">
+          <button className="button" type="button" onClick="onIncrease">
+            Click Me!
+          </button>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default ClickCounter
